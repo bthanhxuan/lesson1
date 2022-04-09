66dca071e1e0063ea74a3677b190ec5013ad20dd
unknown <bthanhxuan12@gmail.com>
2022-04-09 14:26:10 +0700

    second

--- main.js
+++ main.js
@@ -1,3 +1,75 @@
-for (var i = 100; i >= 0; i -= 5) {
-    console.log(i);
+var courseApi = 'http://localhost:3000/courses';
+
+function start() {
+    getCourses(renderCourses);
+
+    handleCreateForm();
 }
+
+start();
+
+//Functions
+function getCourses(callback) {
+    fetch(courseApi)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(callback);
+}
+
+function createCourse(data, callback) {
+    var options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    };
+    fetch(courseApi, options)
+        .then(function (response) {
+            response.json();
+        })
+        .then(callback);
+}
+
+function handleDeleteCourse(id) {
+    var options = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json'}
+    };
+    fetch(courseApi + '/' + id, options)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(){
+            getCourses(renderCourses);
+        });
+}
+
+function renderCourses(courses) {
+    var listCoursesBlock = document.querySelector('#list-courses');
+    var htmls = courses.map(function(course){
+        return `
+            <li>
+                <h4>${course.name}</h4>
+                <p>${course.description}</p>
+                <button onclick="handleDeleteCourse(${course.id})">Xoá</button>
+            </li>  
+        `;
+    });
+    listCoursesBlock.innerHTML = htmls.join('');
+}
+
+function handleCreateForm() {
+    var createBtn = document.querySelector('#create');
+    createBtn.onclick = function() {
+        var name = document.querySelector('input[name="name"]').value;
+        var description = document.querySelector('input[name="description"]').value;
+
+        var formData = {
+            name: name,
+            description: description
+        };
+        createCourse(formData, function() {
+            getCourses(renderCourses);
+        });
+    }
+};
